6dabffd2f9b5c9e03abdad5ff190d55ef665ddfa
Nattfarinn <nattfarinn@gmail.com>
2022-01-26 11:11:56 +0100

    IBX-2061: Rebranded REST route prefix in ibexa/compatibility-layer repository

---
+++ src/lib/Event/Subscriber/RestPrefixSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\CompatibilityLayer\Event\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+final class RestPrefixSubscriber implements EventSubscriberInterface
+{
+    private const LEGACY_REST_PREFIX = '/api/ezp/v2';
+    private const IBEXA_REST_PREFIX = '/api/ibexa/v2';
+
+    private HttpKernelInterface $kernel;
+
+    public function __construct(HttpKernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RequestEvent::class => ['onKernelRequest', 2000],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (strpos($request->getRequestUri(), self::LEGACY_REST_PREFIX) === 0) {
+            $newRequest = $request->duplicate(
+                null,
+                null,
+                null,
+                null,
+                null,
+                array_merge($_SERVER, [
+                    'REQUEST_URI' => str_replace(
+                        self::LEGACY_REST_PREFIX,
+                        self::IBEXA_REST_PREFIX,
+                        $request->getRequestUri()
+                    )
+                ])
+            );
+            $event->setResponse(
+                $this->kernel->handle(
+                    $newRequest,
+                    $event->isMainRequest()
+                        ? HttpKernelInterface::MAIN_REQUEST
+                        : HttpKernelInterface::SUB_REQUEST
+                )
+            );
+            $event->stopPropagation();
+        }
+    }
+}
