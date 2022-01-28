3da4cf56429d6d291466210a25b3dd5e7173c3c5
Andrew Longosz <alongosz@users.noreply.github.com>
2022-01-28 06:57:21 +0100

    IBX-1696: Created Config Resolver namespaces map

---
+++ src/bundle/Resources/mappings/config-resolver-namespaces-map.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+return [
+    'ezsettings' => 'ibexa.site_access.config',
+    'ezrecommendation' => 'ibexa.personalization.site_access.config',
+    'ses_basket' => 'ibexa.commerce.site_access.config.basket',
+    'ses_stored_basket' => 'ibexa.commerce.site_access.config.basket.stored',
+    'ses_eshop' => 'ibexa.commerce.site_access.config.eshop',
+    'silver_eshop' => 'ibexa.commerce.site_access.config.eshop',
+    'ses_forms' => 'ibexa.commerce.site_access.config.forms',
+    'ses_wishlist' => 'ibexa.commerce.site_access.config.wishlist',
+    'silver_content' => 'ibexa.commerce.site_access.config.content',
+    'silver_econtent' => 'ibexa.commerce.site_access.config.econtent',
+    'siso_checkout' => 'ibexa.commerce.site_access.config.checkout',
+    'siso_control_center' => 'ibexa.commerce.site_access.config.control_center',
+    'siso_comparison' => 'ibexa.commerce.site_access.config.comparison',
+    'siso_core' => 'ibexa.commerce.site_access.config.core',
+    'siso_datatypes' => 'ibexa.commerce.site_access.config.data_types',
+    'siso_erp' => 'ibexa.commerce.site_access.config.erp',
+    'siso_local_order_management' => 'ibexa.commerce.site_access.config.order.management.local',
+    'siso_newsletter' => 'ibexa.commerce.site_access.config.newsletter',
+    'siso_order_history' => 'ibexa.commerce.site_access.config.order.history',
+    'siso_pattern_zip' => 'ibexa.commerce.site_access.config.zip',
+    'siso_price' => 'ibexa.commerce.site_access.config.price',
+    'siso_quickorder' => 'ibexa.commerce.site_access.config.quick_order',
+    'siso_rest' => 'ibexa.commerce.site_access.config.rest',
+    'siso_search' => 'ibexa.commerce.site_access.config.search',
+    'siso_seo' => 'ibexa.commerce.site_access.config.seo',
+    'siso_tools' => 'ibexa.commerce.site_access.config.tools',
+    'silver_tools' => 'ibexa.commerce.site_access.config.tools',
+    'siso_voucher' => 'ibexa.commerce.site_access.config.voucher',
+    'st_ez_helper_create_user' => 'ibexa.commerce.site_access.config.create_user',
+];
