31109c67b7dd666fe6d4d3199a68b8ba5c70d139
Dawid Parafiński <dawid.parafinski@ez.no>
2022-02-17 13:57:05 +0100

    IBX-1543: Added twig functions/filters rebranding command

--- src/lib/Rebranding/TwigRebranding.php
+++ src/lib/Rebranding/TwigRebranding.php
@@ -8,8 +8,21 @@ declare(strict_types=1);
 
 namespace Ibexa\CompatibilityLayer\Rebranding;
 
+use Ibexa\Bundle\CompatibilityLayer\IbexaCompatibilityLayerBundle;
+
 class TwigRebranding extends ResourceRebranding
 {
+    private array $twigFunctions;
+
+    private array $twigFilters;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->twigFunctions = require IbexaCompatibilityLayerBundle::MAPPINGS_PATH . \DIRECTORY_SEPARATOR . 'twig-functions-map.php';
+        $this->twigFilters = require IbexaCompatibilityLayerBundle::MAPPINGS_PATH . \DIRECTORY_SEPARATOR . 'twig-filters-map.php';
+    }
+
     public function getFileNamePatterns(): array
     {
         return [
@@ -21,7 +34,40 @@ class TwigRebranding extends ResourceRebranding
     {
         $output = parent::rebrand($input);
 
-        $output = str_replace(array_keys($this->routeNamesMap), array_values($this->routeNamesMap), $output);
+        $output = $this->rebrandRoutes($output);
+        $output = $this->rebrandTwigFunctions($output);
+        $output = $this->rebrandTwigFilters($output);
+
+        return $output;
+    }
+
+    private function rebrandRoutes(string $output): string
+    {
+        return str_replace(array_keys($this->routeNamesMap), array_values($this->routeNamesMap), $output);
+    }
+
+    private function rebrandTwigFunctions(string $output): string
+    {
+        foreach ($this->twigFunctions as $oldFunction => $newFunction) {
+            $output = preg_replace(
+                '/' . $oldFunction . '\(/m',
+                '${1}' . $newFunction . '(',
+                $output
+            );
+        }
+
+        return $output;
+    }
+
+    private function rebrandTwigFilters(string $output): string
+    {
+        foreach ($this->twigFilters as $oldFilter => $newFilter) {
+            $output = preg_replace(
+                '/\|' . $oldFilter . '/m',
+                '${1}' . '|' .$newFilter ,
+                $output
+            );
+        }
 
         return $output;
     }
