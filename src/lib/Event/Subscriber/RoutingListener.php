db5c32262728acad594124eb2d4d6dbce05f93d3
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2023-07-14 16:46:32 +0200

    IBX-5929: Added `RoutingListener` to properly set parameters for `UrlAliasRouter`

---
+++ src/lib/Event/Subscriber/RoutingListener.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\CompatibilityLayer\Event\Subscriber;
+
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Core\MVC\Symfony\Event\PostSiteAccessMatchEvent;
+use Ibexa\Core\MVC\Symfony\MVCEvents;
+use Ibexa\Core\MVC\Symfony\Routing\Generator;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class RoutingListener implements EventSubscriberInterface
+{
+    private ConfigResolverInterface $configResolver;
+
+    private RouterInterface $urlAliasRouter;
+
+    private Generator $urlAliasGenerator;
+
+    public function __construct(
+        ConfigResolverInterface $configResolver,
+        RouterInterface $urlAliasRouter,
+        Generator $urlAliasGenerator
+    ) {
+        $this->configResolver = $configResolver;
+        $this->urlAliasRouter = $urlAliasRouter;
+        $this->urlAliasGenerator = $urlAliasGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MVCEvents::SITEACCESS => ['onSiteAccessMatch', 200],
+        ];
+    }
+
+    public function onSiteAccessMatch(PostSiteAccessMatchEvent $event): void
+    {
+        $rootLocationId = $this->configResolver->getParameter('content.tree_root.location_id');
+        $this->urlAliasRouter->setRootLocationId($rootLocationId);
+        $this->urlAliasGenerator->setRootLocationId($rootLocationId);
+        $this->urlAliasGenerator->setExcludedUriPrefixes(
+            $this->configResolver->getParameter('content.tree_root.excluded_uri_prefixes')
+        );
+    }
+}
